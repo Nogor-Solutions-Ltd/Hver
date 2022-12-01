241e3fdc7db93f740869072dc03f652cef52f833
Sarfaraz Muhammad Sajib <50302555+Muhammad-Sarfaraz@users.noreply.github.com>
2022-12-01 10:35:38 +0600

    Feat/zip (#4)
    * add_command_feature
    * feat/zip
    
    Co-authored-by: sarfaraz muhammad <sarfaraz.scp@gmail.com>

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace NogorSolutionsLTD\Hver;
 
+use Illuminate\Support\Str;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
@@ -23,17 +24,39 @@ class ServiceProvider extends BaseServiceProvider
      */
     public function boot()
     {
-        //
-        
+        # Load all helper functions.
+        foreach ( scandir( __DIR__ . DIRECTORY_SEPARATOR . 'helpers' ) as $helperFile ) {
+            $path = sprintf(
+                '%s%s%s%s%s',
+                __DIR__,
+                DIRECTORY_SEPARATOR,
+                'helpers',
+                DIRECTORY_SEPARATOR,
+                $helperFile
+            );
+
+            if ( !is_file( $path ) ) {
+                continue;
+            }
+
+            $function = Str::before( $helperFile, '.php' );
+
+            if ( function_exists( $function ) ) {
+                continue;
+            }
+
+            require_once $path;
+        }
+
         if ($this->app->runningInConsole()) {
             $this->commands([
-                \NogorSolutionsLTD\Hver\Console\Commands\SetLocalCommand::class,
+                \NogorSolutionsLTD\Hver\Console\Commands\EnvironmentCommand::class,
                 \NogorSolutionsLTD\Hver\Console\Commands\SetProdCommand::class,
                 \NogorSolutionsLTD\Hver\Console\Commands\SetWebPackProdCommand::class,
                 \NogorSolutionsLTD\Hver\Console\Commands\SetWebPackLocalCommand::class,
             ]);
         }
-        
+
         $this->publishes([
             __DIR__.'/../config/hver.php' => config_path('hver.php'),
         ]);
